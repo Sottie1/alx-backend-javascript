9104575a076d13522b029ff5dbef1b5eac12881d
Sottie1 <sottiegadson@gmail.com>
2023-01-26 20:36:25 +0000

    Initial commit

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,28 +4,11 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-  get size() {
+  valueOf() {
     return this._size;
-  }
-
-  set size(value) {
-    this._size = value;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
-    this._location = value;
   }
 
   toString() {
     return this._location;
   }
-
-  valueOf() {
-    return this._size;
-  }
 }
-
